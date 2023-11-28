275bbdaf675e2672d69cb95354e1b53829357eca
Nneoma Amechi <amechinneoma53@gmail.com>
2023-11-28 01:04:30 -0500

    Updates

--- script.js
+++ script.js
@@ -41,3 +41,48 @@ function inititialise() {
 // Inits & Event Listeners
 //
 inititialise();
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value == "") {
+    alert("Please enter a task");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "❌";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData()
+  }
+  else if (e.target.tagName === "SPAN") {
+    e.target.parentNode.remove();
+    saveData()
+  }
+}, false);
+
+function saveData() {
+  localStorage.setItem("listContainer", listContainer.innerHTML);
+}
+
+function loadData() {
+  if (localStorage.getItem("listContainer")) {
+    listContainer.innerHTML = localStorage.getItem("listContainer");
+  }
+}
+
+loadData();
+
+// Citations and References: https://m.youtube.com/watch?v=G0jO8kUrg-I&pp=ygUGI3l1ZGl2
+// GreatStack March 20, 2023
